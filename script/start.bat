bcfa9344141322f2fc36bfa08fec517d2536cd4e
Chakwen Hong <swgrhck@gmail.com>
2018-09-28 22:19:52 +0800

    start.bat: check whether aria2 is already running before calling start.vbs

--- script/start.bat
+++ script/start.bat
@@ -1,4 +1,13 @@
-@echo off & title Aria2 Start & setlocal
+@echo off & title Aria2 Start & setlocal EnableDelayedExpansion
+
+tasklist /nh /fi "imagename eq aria2c.exe" 2> nul | find /i "aria2c.exe" > nul 2> nul
+if %errorlevel%==0 (
+    :option
+    echo Aria2c is already running, do you want to start a new process?
+    choice /c YNS /m "[Y:Yes; N:No; P:Print aria2 processes]"
+    if !errorlevel!==2 pause && exit /b
+    if !errorlevel!==3 tasklist /fi "imagename eq aria2c.exe" & echo. & goto option
+)
 
 set dir=%~dp0
 call cscript %dir%\start.vbs > nul 2> nul
